b090a1b733ce1a715ecb7b5f1ce15b90cfff5074
N-Reptyl <nassim.mourabit@outlook.fr>
2025-08-15 21:16:44 +0200

    add color themse

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,18 @@ export default {
   ], 
 	theme:{
 		extend:{
-			colors:{
-				darkbg:"#0B1D3B",
-				accent:"#3B9CFF"
-			},
+			colors: {
+        primary: '#255147',     // Vert foncé (boutons, fonds)
+		primary_hover: '#477268',
+        secondary: '#3a7f6f',   // Vert clair (accents, dégradés)
+		secondary_hover: '#3f937f',
+        accent: '#183831',      // Vert très sombre (titres, textes)
+        light: '#ffffff',       // Blanc (textes clairs)
+        dark: '#0B1D3B',        // Bleu nuit utilisé pour ombres / textes
+      },
+      backgroundImage: {
+        'hero-gradient': 'radial-gradient(circle, #3a7f6f 0%, #255147 60%)',
+      },
 			fontFamily: {
 				'roboto': ['Roboto'],
 			}
